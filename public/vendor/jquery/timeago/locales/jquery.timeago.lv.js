8390879366093f87129dac6ea59300540c8f2d07
Filips Alpe <filips@akeneo.com>
2014-08-09 13:56:17 +0300

    Add lv

---
+++ public/vendor/jquery/timeago/locales/jquery.timeago.lv.js
@@ -0,0 +1,20 @@
+// Latvian
+jQuery.timeago.settings.strings = {
+  prefixAgo: "",
+  prefixFromNow: "",
+  suffixAgo: "",
+  suffixFromNow: "",
+  seconds: "tikko",
+  minute: "pirms minūtes",
+  minutes: "pirms %d minūtēm",
+  hour: "pirms stundas",
+  hours: "pirms %d stundām",
+  day: "vakar",
+  days: "pirms %d dienām",
+  month: "pirms mēneša",
+  months: "pirms %d mēnešiem",
+  year: "pirms gada",
+  years: "pirms %d gadiem",
+  wordSeparator: " ",
+  numbers: []
+};
